03f658f55e2f11e60d1d1571f7164384c7c87b41
Yaron Yamin <yaronyam@users.noreply.github.com>
2016-12-31 15:17:46 +0200

    TestMe popup: html action labels

--- src/main/java/com/weirddev/testme/intellij/TemplateRegistry.java
+++ src/main/java/com/weirddev/testme/intellij/TemplateRegistry.java
@@ -18,15 +18,15 @@ public class TemplateRegistry {
 
     static {
 /*
-        //html version:
+        //html version (has issues with horizontal alignment of img to text in JLabel):
         URL juDark = TemplateRegistry.class.getResource("/icons/junit_dark.png");
         URL mockito = TemplateRegistry.class.getResource("/icons/mockito.png");
         URL ju5 = TemplateRegistry.class.getResource("/icons/junit5.png");
         templateDescriptors.add(new TemplateDescriptor("<html>with <i><b>JUnit4</b></i><img src='"+ juDark +"'> & <i><b>Mockito</b></i><img src='"+ mockito +"'></html>", "TestMe with JUnit4 & Mockito.java"));
         templateDescriptors.add(new TemplateDescriptor("<html>with <i><b>JUnit5</b></i><img src='"+ ju5 +"'> & <i><b>Mockito</b></i><img src='"+ mockito +"'></html>", "TestMe with JUnit4 & Mockito.java"));
 */
-        templateDescriptors.add(new TemplateDescriptor("with JUnit4<JUnit4>& Mockito<Mockito>", JUNIT4_MOCKITO_JAVA_TEMPLATE));
-        templateDescriptors.add(new TemplateDescriptor("with JUnit5<JUnit5>& Mockito<Mockito>", JUNIT5_MOCKITO_JAVA_TEMPLATE));
+        templateDescriptors.add(new TemplateDescriptor("<html>with <i>JUnit4</i></html><JUnit4><html>& <i>Mockito</i></html><Mockito>", JUNIT4_MOCKITO_JAVA_TEMPLATE));
+        templateDescriptors.add(new TemplateDescriptor("<html>with <i>JUnit5</i></html><JUnit5><html>& <i>Mockito</i></html><Mockito>", JUNIT5_MOCKITO_JAVA_TEMPLATE));
     }
     public List<TemplateDescriptor> getTemplateDescriptors(){
         return templateDescriptors;
