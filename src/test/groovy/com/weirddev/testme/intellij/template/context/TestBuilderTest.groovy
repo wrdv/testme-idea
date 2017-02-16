45031b9d042380dac46a25fddc856263144eb0b0
Yaron Yamin <yaronyam@users.noreply.github.com>
2017-02-16 13:42:58 +0200

    template macro refactoring : TestBuilder migrate UT to spock

--- src/test/groovy/com/weirddev/testme/intellij/template/context/TestBuilderTest.groovy
+++ src/test/groovy/com/weirddev/testme/intellij/template/context/TestBuilderTest.groovy
@@ -1,33 +1,67 @@
 package com.weirddev.testme.intellij.template.context
 
+import spock.lang.Specification
+
 /**
  * Date: 2/16/2017
  * @author Yaron Yamin
  */
-class TestBuilderTest extends GroovyTestCase {
-    void testStripGenerics() {
-        assert "java.util.Set" ==  new TestBuilder().stripGenerics("java.util.Set<Fire>")
-    }
-    void testReplaceType() {
-        assert "java.util.HashSet<Fire>" ==  new TestBuilder().replaceType("java.util.Set<Fire>","java.util.HashSet%1\$s")
-    }
-    void testReplaceTypeWithNesting() {
-        assert "java.util.HashSet<List<Fire>>" ==  new TestBuilder().replaceType("java.util.Set<List<Fire>>","java.util.HashSet%1\$s")
-    }
-    void testReplaceTypeWithoutGenerics() {
-        assert "java.util.Arrays.asList" ==  new TestBuilder().replaceType("java.util.Set<Fire>","java.util.Arrays.asList")
-    }
-    void testReplaceTypeClass() {
-        assert "HashSet<Fire>" ==  new TestBuilder().replaceType("Set<Fire>","HashSet%1\$s")
+class TestBuilderTest extends Specification {
+
+    def "stripGenerics"() {
+        given:
+        def testBuilder = new TestBuilder()
+
+        expect:
+        testBuilder.stripGenerics(canonicalName) == result
+
+        where:
+        canonicalName               | result
+        "java.util.Set"             | "java.util.Set"
+        "java.util.Set<Fire>"       | "java.util.Set"
+        "java.util.Set<List<Fire>>" | "java.util.Set"
     }
 
-    void testExtractGenerics() {
-        assert "<List<Fire>>" ==  new TestBuilder().extractGenerics("java.util.Set<List<Fire>>")
+    def "extractGenerics"() {
+        given:
+        def testBuilder = new TestBuilder()
+
+        expect:
+        testBuilder.extractGenerics(canonicalName) == result
+
+        where:
+        canonicalName               | result
+        "java.util.Set"             | ""
+        "java.util.Set<Fire>"       | "<Fire>"
+        "java.util.Set<List<Fire>>" | "<List<Fire>>"
     }
-    void testResolveTypeName() {
-        assert "java.util.HashSet" ==  new TestBuilder().resolveTypeName(new Type("java.util.Set<List<Fire>>","Set","java.util",false,false,[]),["java.util.Set":"java.util.HashSet"])
+
+    def "replaceType"() {
+        given:
+        def testBuilder = new TestBuilder()
+
+        expect:
+        testBuilder.replaceType(canonicalName, replacementType) == result
+
+        where:
+        result                          | canonicalName               | replacementType
+        "java.util.HashSet<Fire>"       | "java.util.Set<Fire>"       | "java.util.HashSet%1\$s"
+        "java.util.HashSet<List<Fire>>" | "java.util.Set<List<Fire>>" | "java.util.HashSet%1\$s"
+        "java.util.Arrays.asList"       | "java.util.Set<Fire>"       | "java.util.Arrays.asList"
+        "HashSet<Fire>"                 | "Set<Fire>"                 | "HashSet%1\$s"
     }
-    void testResolveTypeNameWithGenerics() {
-        assert "java.util.HashSet<List<Fire>>" ==  new TestBuilder().resolveTypeName(new Type("java.util.Set<List<Fire>>","Set","java.util",false,false,[]),["java.util.Set":"java.util.HashSet%1\$s"])
+
+    def "resolveTypeName"() {
+        expect:
+        result == new TestBuilder().resolveTypeName(new Type(canonicalName, "Set", "java.util", false, false, []), replacementMap as HashMap)
+
+        where:
+        result                          | canonicalName               | replacementMap
+        "java.util.HashSet<Fire>"       | "java.util.Set<Fire>"       | ["java.util.Set": "java.util.HashSet%1\$s"]
+        "java.util.HashSet<List<Fire>>" | "java.util.Set<List<Fire>>" | ["java.util.Set": "java.util.HashSet%1\$s"]
+        "java.util.Set<List<Fire>>"     | "java.util.Set<List<Fire>>" | ["java.util.SetZ": "java.util.HashSet%1\$s"]
+        "java.util.Set<List<Fire>>"     | "java.util.Set<List<Fire>>" | []
+        "java.util.HashSet"             | "java.util.Set<List<Fire>>" | ["java.util.Set": "java.util.HashSet"]
+        "java.util.Arrays.asList"       | "java.util.Set<Fire>"       | ["java.util.Set": "java.util.Arrays.asList"]
     }
 }
