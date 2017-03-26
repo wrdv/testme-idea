4e62c7d44230e80c11fad64e1c57dcf9810eeba9
Yaron Yamin <yaronyam@users.noreply.github.com>
2017-03-26 23:20:08 +0300

    test templates: add Spock template with ui action

---
+++ src/integrationTest/java/com/weirddev/testme/intellij/generator/TestMeGeneratorSpockTest.java
@@ -0,0 +1,21 @@
+package com.weirddev.testme.intellij.generator;
+
+import com.weirddev.testme.intellij.template.TemplateRegistry;
+
+/**
+ * Date: 24/02/2017
+ *
+ * @author Yaron Yamin
+ */
+public class TestMeGeneratorSpockTest extends TestMeGeneratorTestBase {
+    public TestMeGeneratorSpockTest() {
+        super(TemplateRegistry.SPOCK_GROOVY_MOCKITO_JAVA_TEMPLATE, "testSpock");
+        expectedTestClassExtension = "groovy";
+    }
+    public void testBean() throws Exception{
+        doTest();
+    }
+    public void testCtorOverProps() throws Exception{
+        doTest(true,true,true);
+    }
+}
