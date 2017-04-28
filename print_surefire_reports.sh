89a1eb50353230fde7cee883f36c86aa5747b90d
Yaron Yamin <yaronyam@users.noreply.github.com>
2017-04-28 10:14:18 +0300

    travis: try to print better test reporting. take #3

--- print_surefire_reports.sh
+++ print_surefire_reports.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env sh
 echo "Current directory is $(pwd)"
 echo "\n=== SUREFIRE REPORTS ===\n"
-
-for F in target/surefire-reports/*.txt
+for F in build/test-results/test/*.xml
 do
-    echo $F
+    echo $F:
+    cat $F
+    echo
+done
+for F in build/integrationTest-results/*.xml
+do
+    echo $F:
     cat $F
     echo
 done
