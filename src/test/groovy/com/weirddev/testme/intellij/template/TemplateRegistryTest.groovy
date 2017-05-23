bc3edc4a7d7a0d906f7167a57a67506b62539730
Yaron Yamin <yaronyam@users.noreply.github.com>
2017-05-23 21:55:27 +0300

    TestNG framework support: fix UT

--- src/test/groovy/com/weirddev/testme/intellij/template/TemplateRegistryTest.groovy
+++ src/test/groovy/com/weirddev/testme/intellij/template/TemplateRegistryTest.groovy
@@ -10,10 +10,11 @@ class TemplateRegistryTest {
     @Test
     void testTemplateRegistry() throws Exception {
         def descriptors = new TemplateRegistry().getTemplateDescriptors()
-        assert descriptors.size() ==4
+        assert descriptors.size() ==5
         assert descriptors.find({it.tokenizedDisplayName.contains('JUnit4')&& it.tokenizedDisplayName.contains('Groovy')}).filename == TemplateRegistry.JUNIT4_GROOVY_MOCKITO_JAVA_TEMPLATE
         assert descriptors.find({it.tokenizedDisplayName.contains('Spock')&& it.tokenizedDisplayName.contains('Groovy')}).filename == TemplateRegistry.SPOCK_GROOVY_MOCKITO_JAVA_TEMPLATE
         assert descriptors.find({it.tokenizedDisplayName.contains('JUnit5')&& it.tokenizedDisplayName.contains('Mockito')}).filename == TemplateRegistry.JUNIT5_MOCKITO_JAVA_TEMPLATE
+        assert descriptors.find({it.tokenizedDisplayName.contains('TestNG')&& it.tokenizedDisplayName.contains('Mockito')}).filename == TemplateRegistry.TESTNG_MOCKITO_JAVA_TEMPLATE
         assert descriptors.find({it.tokenizedDisplayName.contains('JUnit5')&& it.tokenizedDisplayName.contains('Mockito')}).displayName.find("JUnit5.*Mockito")!=null
     }
 }
