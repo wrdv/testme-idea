158dfc260c097cfde013f2f89b40000659990edc
Yaron Yamin <yaronyam@users.noreply.github.com>
2018-01-02 00:24:09 +0200

    scala specs2: support future types. part #1

--- testData/testMeGenerator/scalaFuture/testSpecs2/com/example/services/impl/FooTest.scala
+++ testData/testMeGenerator/scalaFuture/testSpecs2/com/example/services/impl/FooTest.scala
@@ -2,15 +2,24 @@ package com.example.services.impl
 
 import org.specs2.mutable.Specification
 
+import scala.concurrent.duration._
+import scala.concurrent.{Await, Future}
+import scala.concurrent.ExecutionContext.Implicits.global
+
 /** created by TestMe integration test on MMXVI */
 class FooTest extends Specification {
   val foo = new com.example.services.impl.Foo()
 
   "Foo" should {
 
-    "example" in {
-      val result = foo.example((Some(1), Some(List(List(11223344)))))
-      result === "replaceMeWithExpectedResult"
+    "look Into The Future" in {
+      val result = foo.lookIntoTheFuture(Future(1))
+      Await.result(result, 10.seconds) === 1.1f
+    }
+
+    "find Me A Better Future" in {
+      val result = foo.findMeABetterFuture(Some("hopes"))
+      Await.result(result, 10.seconds) === "result"
     }
 
   }
