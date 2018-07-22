2133780ccd4f2d47288810ebbeb124f8bf8a3fc7
Yaron Yamin <yaronyam@users.noreply.github.com>
2018-07-23 00:19:21 +0300

    specs2: fix compatability with IDEA 2018

--- testData/testMeGenerator/variousTypesOfArguments/testSpecs2/com/example/services/impl/FooTest.scala
+++ testData/testMeGenerator/variousTypesOfArguments/testSpecs2/com/example/services/impl/FooTest.scala
@@ -1,22 +1,19 @@
 package com.example.services.impl
 
-import com.example.foes.Fire
-import com.example.services.impl.Foo.InnerStaticClass
-import com.example.warriers.FooFighter
 import org.specs2.mutable.Specification
 import org.specs2.mock.Mockito
 
 /** created by TestMe integration test on MMXVI */
 class FooTest extends Specification with Mockito {
   isolated
-  val fooFighter: FooFighter = mock[FooFighter]
-  val innerStaticClass: InnerStaticClass = mock[InnerStaticClass]
+  val fooFighter: com.example.warriers.FooFighter = mock[com.example.warriers.FooFighter]
+  val innerStaticClass: com.example.services.impl.Foo.InnerStaticClass = mock[com.example.services.impl.Foo.InnerStaticClass]
   val foo = new Foo(fooFighter, 64.toByte, 1, 12345l, 1.1f, 100d, true, 'c', 21.toShort, innerStaticClass)
 
   "Foo" should {
 
     "various Args" in {
-      val result = foo.variousArgs(new Fire(), "foeName", 64.toByte, 1, 12345l, 1.1f, 100d, true, 'c', 21.toShort, BigDecimal(2.44).bigDecimal, BigInt(123123).bigInteger, new java.util.GregorianCalendar(2016, java.util.Calendar.JANUARY, 11, 22, 45).getTime, java.time.LocalDateTime.of(2016, java.time.Month.JANUARY, 11, 22, 45, 55).toInstant(java.time.ZoneOffset.UTC))
+      val result = foo.variousArgs(new com.example.foes.Fire(), "foeName", 64.toByte, 1, 12345l, 1.1f, 100d, true, 'c', 21.toShort, BigDecimal(2.44).bigDecimal, BigInt(123123).bigInteger, new java.util.GregorianCalendar(2016, java.util.Calendar.JANUARY, 11, 22, 45).getTime, java.time.LocalDateTime.of(2016, java.time.Month.JANUARY, 11, 22, 45, 55).toInstant(java.time.ZoneOffset.UTC))
       result === "replaceMeWithExpectedResult"
     }
 
