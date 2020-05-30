ce506d464efe61f61b5b064898d8c540fc28f972
Yaron Yamin <yaronyam@users.noreply.github.com>
2020-05-30 09:46:59 +0300

    configurable templates. UI - added template configuration page. copied implementation from IDEA File templates config

---
+++ src/main/java/com/weirddev/testme/intellij/ui/template/ExportableFileTemplateSettings.java
@@ -0,0 +1,21 @@
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.weirddev.testme.intellij.ui.template;
+
+
+
+import com.intellij.openapi.components.State;
+import com.intellij.openapi.components.Storage;
+
+/**
+ * @author Dmitry Avdeev
+ */
+@State(
+  name = "ExportableFileTemplateSettings",
+  storages = @Storage(FileTemplateSettings.EXPORTABLE_SETTINGS_FILE),
+  additionalExportFile = FileTemplatesLoader.TEMPLATES_DIR
+)
+class ExportableFileTemplateSettings extends FileTemplateSettings {
+  ExportableFileTemplateSettings() {
+    super(null);
+  }
+}
