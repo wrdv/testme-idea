231a6af9fd2d3a2904ce6cd49120a6556d7ee697
Yaron Yamin <yaronyam@users.noreply.github.com>
2020-09-05 16:55:41 +0300

    configurable templates. UI - added template configuration page. make OOB templates readonly and un-removable

--- src/main/java/com/weirddev/testme/intellij/ui/template/ExportableFileTemplateSettings.java
+++ src/main/java/com/weirddev/testme/intellij/ui/template/ExportableFileTemplateSettings.java
@@ -1,7 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.weirddev.testme.intellij.ui.template;
-
-
 
 import com.intellij.openapi.components.State;
 import com.intellij.openapi.components.Storage;
