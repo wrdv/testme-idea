e6331767017ce9126cdff0e4625d5e10fa8002e2
Yaron Yamin <yaronyam@users.noreply.github.com>
2021-04-17 23:40:57 +0300

    IDEA 2021 compatibility: fix incompatibility issue. for test code as well

--- testme-intellij-groovy/src/main/java/com/weirddev/testme/intellij/groovy/resolvers/GroovyPropertyUtil.java
+++ testme-intellij-groovy/src/main/java/com/weirddev/testme/intellij/groovy/resolvers/GroovyPropertyUtil.java
@@ -5,13 +5,10 @@ import org.jetbrains.annotations.Nullable;
 import org.jetbrains.plugins.groovy.lang.psi.util.GroovyPropertyUtils;
 
 /**
- * Created with IntelliJ IDEA.
+ * A wrapper to IJ GroovyPropertyUtils, used to isolate groovy plugin so there will be not attempts to load it at runtime when its not available
  * User: yaron.yamin
  * Date: 11/1/2017
  * Time: 4:23 PM
- */
-/**
-A this wrapper to IJ GroovyPropertyUtils, used to isolate groovy plugin so there will be not attempts to load it at runtime when its not available
  */
 public class GroovyPropertyUtil
 {
