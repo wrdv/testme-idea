762a9ba78912d28e313e111949e877a7c3bc25a9
Yaron Yamin <yaronyam@users.noreply.github.com>
2021-04-19 18:11:44 +0300

    IDEA 2021.1 compatibility: fix IUT - handle re-occurring velocity initializations at a later stage ( post startup)

--- src/main/java/com/weirddev/testme/intellij/utils/AccessLevelReflectionUtils.java
+++ src/main/java/com/weirddev/testme/intellij/utils/AccessLevelReflectionUtils.java
@@ -1,7 +1,7 @@
 package com.weirddev.testme.intellij.utils;
 
 import java.lang.reflect.Field;
-import java.lang.reflect.Modifier;
+
 /**
  * Date: 2/13/2017
  *
@@ -11,5 +11,9 @@ public class AccessLevelReflectionUtils {
     public static void replaceField(Field field, Object newValue) throws Exception {
         field.setAccessible(true);
         field.set(null, newValue);
+    }
+    public static Object getField(Field field, Object obj) throws Exception {
+        field.setAccessible(true);
+        return field.get(obj);
     }
 }
