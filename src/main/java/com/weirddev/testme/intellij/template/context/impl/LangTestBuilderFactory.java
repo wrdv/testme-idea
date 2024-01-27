385d1829bb56a5d5aecdcdc7d1ba2534b4520d9b
Yaron Yamin <yaronyam@users.noreply.github.com>
2024-01-27 18:31:33 +0200

    refactor: mark javaVersion as @Nullable

--- src/main/java/com/weirddev/testme/intellij/template/context/impl/LangTestBuilderFactory.java
+++ src/main/java/com/weirddev/testme/intellij/template/context/impl/LangTestBuilderFactory.java
@@ -6,17 +6,19 @@ import com.weirddev.testme.intellij.template.FileTemplateConfig;
 import com.weirddev.testme.intellij.template.TypeDictionary;
 import com.weirddev.testme.intellij.template.context.*;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Map;
 
 public class LangTestBuilderFactory {
-    private Language language;
-    private FileTemplateConfig fileTemplateConfig;
-    private Module srcModule;
-    private TypeDictionary typeDictionary;
-    private JavaVersion javaVersion;
+    private final Language language;
+    private final FileTemplateConfig fileTemplateConfig;
+    private final Module srcModule;
+    private final TypeDictionary typeDictionary;
+    @Nullable
+    private final JavaVersion javaVersion;
 
-    public LangTestBuilderFactory(Language language, Module srcModule, FileTemplateConfig fileTemplateConfig, TypeDictionary typeDictionary, JavaVersion javaVersion) {
+    public LangTestBuilderFactory(Language language, Module srcModule, FileTemplateConfig fileTemplateConfig, TypeDictionary typeDictionary, @Nullable JavaVersion javaVersion) {
         this.language = language;
         this.fileTemplateConfig = fileTemplateConfig;
         this.srcModule = srcModule;
@@ -25,15 +27,14 @@ public class LangTestBuilderFactory {
     }
 
     @NotNull
-    public LangTestBuilder createTestBuilder(Method method, TestBuilder.ParamRole paramRole, Map<String, String> defaultTypeValues, Map<String, String> typesOverrides) throws Exception {
+    public LangTestBuilder createTestBuilder(Method method, TestBuilder.ParamRole paramRole, Map<String, String> defaultTypeValues, Map<String, String> typesOverrides) {
         LangTestBuilder langTestBuilder;
-        if ( language==Language.Scala) {
-            langTestBuilder = new ScalaTestBuilder(method, paramRole,fileTemplateConfig, srcModule,typeDictionary,javaVersion, defaultTypeValues, typesOverrides);
-        }
-        else if ( language==Language.Groovy) {
-            langTestBuilder = new GroovyTestBuilderImpl(method, paramRole,fileTemplateConfig, srcModule,typeDictionary,javaVersion, defaultTypeValues, typesOverrides);
-        } else{
-            langTestBuilder = new JavaTestBuilderImpl(method, paramRole, fileTemplateConfig, srcModule,typeDictionary,javaVersion, defaultTypeValues, typesOverrides );
+        if (language == Language.Scala) {
+            langTestBuilder = new ScalaTestBuilder(method, paramRole, fileTemplateConfig, srcModule, typeDictionary, javaVersion, defaultTypeValues, typesOverrides);
+        } else if (language == Language.Groovy) {
+            langTestBuilder = new GroovyTestBuilderImpl(method, paramRole, fileTemplateConfig, srcModule, typeDictionary, javaVersion, defaultTypeValues, typesOverrides);
+        } else {
+            langTestBuilder = new JavaTestBuilderImpl(method, paramRole, fileTemplateConfig, srcModule, typeDictionary, javaVersion, defaultTypeValues, typesOverrides);
         }
         return langTestBuilder;
     }
