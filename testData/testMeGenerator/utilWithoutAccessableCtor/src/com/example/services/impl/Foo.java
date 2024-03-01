f35c816afb71e693f2a1a8f6d918d9f38b3357cd
南京-黄亮 <huangl250@chinaunicom.cn>
2024-03-01 18:20:46 +0800

    test modify

--- testData/testMeGenerator/utilWithoutAccessableCtor/src/com/example/services/impl/Foo.java
+++ testData/testMeGenerator/utilWithoutAccessableCtor/src/com/example/services/impl/Foo.java
@@ -5,9 +5,10 @@ import com.example.foes.Fire;
 <caret>
 public class Foo {
 
-    private static final FooFighter fooFighter = new FearFighterImpl();
+    private static FooFighter fooFighter = new FearFighterImpl();
 
     public static String fight(Fire withFire,String foeName) {
+        fooFighter.fight(withFire);
         return foeName;
     }
     
