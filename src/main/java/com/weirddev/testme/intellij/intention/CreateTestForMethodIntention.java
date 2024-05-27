018c1882b14c7ee8af901dd5d6a7a1604e522391
南京-黄亮 <huangl250@chinaunicom.cn>
2024-05-27 09:51:26 +0800

    #40 fix problem, usage of GenerateMissedTestsAction class that became final in 2024.1

--- src/main/java/com/weirddev/testme/intellij/intention/CreateTestForMethodIntention.java
+++ src/main/java/com/weirddev/testme/intellij/intention/CreateTestForMethodIntention.java
@@ -1,14 +1,14 @@
 package com.weirddev.testme.intellij.intention;
 
+import com.intellij.codeInsight.TestFrameworks;
 import com.intellij.codeInsight.hint.HintManager;
+import com.intellij.codeInsight.intention.PsiElementBaseIntentionAction;
 import com.intellij.codeInsight.intention.preview.IntentionPreviewInfo;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
-import com.intellij.psi.PsiClass;
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiFile;
-import com.intellij.psi.PsiMethod;
-import com.intellij.testIntegration.createTest.GenerateMissedTestsAction;
+import com.intellij.psi.*;
+import com.intellij.psi.util.PsiTreeUtil;
+import com.intellij.testIntegration.TestFramework;
 import com.intellij.util.IncorrectOperationException;
 import com.weirddev.testme.intellij.TestMeBundle;
 import com.weirddev.testme.intellij.action.CreateTestMeAction;
@@ -23,7 +23,36 @@ import org.jetbrains.annotations.NotNull;
  * @author huangliang
  */
 @NonNls
-final class CreateTestForMethodIntention extends GenerateMissedTestsAction {
+final class CreateTestForMethodIntention extends PsiElementBaseIntentionAction {
+
+    /**
+     * Checks whether this intention is available at the caret offset in file - the caret must sit a testable method
+     * character in a ternary statement. If this condition is met, this intention's entry is shown in the available
+     * intentions list.
+     * copy from GenerateMissedTestsAction-isAvailable
+     * <p>Note: this method must do its checks quickly and return.</p>
+     *
+     * @param project a reference to the Project object being edited.
+     * @param editor  a reference to the object editing the project source
+     * @param element a reference to the PSI element currently under the caret
+     * @return {@code true} if the caret is in a literal string element, so this functionality should be added to the
+     * intention menu or {@code false} for all other types of caret positions
+     */
+    @Override
+    public boolean isAvailable(@NotNull Project project, Editor editor, @NotNull PsiElement element) {
+        if (!TestFramework.EXTENSION_NAME.hasAnyExtensions()) return false;
+
+        final PsiElement parent = element.getParent();
+        if (!(parent instanceof PsiMethod)) return false;
+
+        if (!((PsiMethod)parent).hasModifierProperty(PsiModifier.PUBLIC) ||
+            ((PsiMethod)parent).hasModifierProperty(PsiModifier.ABSTRACT)) {
+            return false;
+        }
+
+        PsiClass aClass = PsiTreeUtil.getParentOfType(element, PsiClass.class);
+        return aClass != null && TestFrameworks.detectFramework(aClass) == null;
+    }
 
     /**
      * pop up the testMe menu list
@@ -67,6 +96,11 @@ final class CreateTestForMethodIntention extends GenerateMissedTestsAction {
      */
     @NotNull public String getFamilyName() {
         return "TestMe: Generate test for method";
+    }
+
+    @Override
+    public boolean startInWriteAction() {
+        return false;
     }
 
 }
